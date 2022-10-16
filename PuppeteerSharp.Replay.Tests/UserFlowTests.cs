32e2269fce94e054637dab2888f6a4e7b8ac9268
Jason Williams <jason@centrolutions.com>
2022-10-15 21:02:42 -0500

    Add deserialize tests for UserFlow

---
+++ PuppeteerSharp.Replay.Tests/UserFlowTests.cs
@@ -0,0 +1,17 @@
+﻿namespace PuppeteerSharp.Replay.Tests
+{
+    public class UserFlowTests
+    {
+        [Fact]
+        public void Parse_DeserializesCorrectly()
+        {
+            var jsonText = File.ReadAllText("Data\\UserFlowExample.json");
+            
+            var sut = UserFlow.Parse(jsonText);
+
+            Assert.NotNull(sut);
+            Assert.Equal("Google Centrolutions", sut.Title);
+            Assert.Equal(8, sut.Steps.Length);
+        }
+    }
+}
--- PuppeteerSharp.Replay/UserFlow.cs
+++ PuppeteerSharp.Replay/UserFlow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,6 +9,12 @@ namespace PuppeteerSharp.Replay
     {
         public string Title { get; set; }
         public Step[] Steps { get; set; }
+
+        public static UserFlow Parse(string json)
+        {
+            var result = JsonConvert.DeserializeObject<UserFlow>(json);
+            return result;
+        }
     }
 
     public class Step
