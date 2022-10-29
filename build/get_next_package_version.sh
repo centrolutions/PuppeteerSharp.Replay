506ab3382dac515cd7e7130658bf89cd43a94602
Jason Williams <jason@centrolutions.com>
2022-10-29 17:20:42 -0500

    Get right version from nuget

--- build/get_next_package_version.sh
+++ build/get_next_package_version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 package_content=$(curl -s https://api.nuget.org/v3/registration5-semver1/puppeteersharp.replay/index.json)
-package_version=$(echo $package_content | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["items"][0]["items"][0]["catalogEntry"]["version"])')
+package_version=$(echo $package_content | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["items"][0]["upper"])')
 echo ${package_version} | awk -F. '{print $1"."$2"."$3+1}'
